91dae73b8c915d0918392a0e3339f7a786647e05
Shrey008icloud <Shreyans.38434@stu.upes.ac.in>
2026-08-24 12:50:06 +0530

    Add files via upload

---
+++ Day15Q1.c
@@ -0,0 +1,27 @@
+/*Q29 (Loops without Arrays/Strings)
+Write a program to calculate the factorial of a number.
+Input 1:
+5
+Output 1:
+120
+Input 2:
+3
+Output 2:
+6*/
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    long long factorial = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        factorial *= i;
+    }
+
+    printf("Factorial of %d = %lld", n, factorial);
+
+    return 0;
+}

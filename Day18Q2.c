47861d94985c889cd539139ee2200b72d3682e21
Shrey008icloud <Shreyans.38434@stu.upes.ac.in>
2026-08-31 17:03:53 +0530

    Add files via upload

---
+++ Day18Q2.c
@@ -0,0 +1,29 @@
+/*Q36 (Loops without Arrays/Strings)
+Write a program to find the HCF (GCD) of two numbers.
+Show Sample Test Cases
+Input 1:
+12 18
+Output 1:
+6
+Input 2:
+7 9
+Output 2:
+1*/
+#include <stdio.h>
+
+int main() {
+    int a, b, i, hcf;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    for (i = 1; i <= a && i <= b; i++) {
+        if (a % i == 0 && b % i == 0) {
+            hcf = i;
+        }
+    }
+
+    printf("HCF (GCD) = %d", hcf);
+
+    return 0;
+}

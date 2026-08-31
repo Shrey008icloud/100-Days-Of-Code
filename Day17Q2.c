1d97bf0087482211e53819b3ae272d417469fddb
Shrey008icloud <Shreyans.38434@stu.upes.ac.in>
2026-08-31 17:01:11 +0530

    Add files via upload

---
+++ Day17Q2.c
@@ -0,0 +1,37 @@
+/*Q34 (Loops without Arrays/Strings)
+Write a program to check if a number is prime.
+Show Sample Test Cases
+Input 1:
+7
+Output 1:
+Prime
+Input 2:
+10
+Output 2:
+Not prime*/
+#include <stdio.h>
+
+int main() {
+    int n, i, isPrime = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n <= 1) {
+        isPrime = 0;
+    } else {
+        for (i = 2; i < n; i++) {
+            if (n % i == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+    }
+
+    if (isPrime)
+        printf("The number is prime.");
+    else
+        printf("The number is not prime.");
+
+    return 0;
+}

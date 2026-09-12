6a3085d045ffce77d2b6dc7a7251821eebfb6e96
Shrey008icloud <Shreyans.38434@stu.upes.ac.in>
2026-09-12 11:32:24 +0530

    Add files via upload

---
+++ Day34Q2.c
@@ -0,0 +1,36 @@
+/*Q68 (Arrays (1D))
+Delete an element from an array.
+Show Sample Test Cases
+Input 1:
+5
+1 2 3 4 5
+2
+Output 1:
+1 2 4 5
+*/
+#include <stdio.h>
+
+int main() {
+    int a[100], n, pos, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements:\n");
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    printf("Enter position to delete: ");
+    scanf("%d", &pos);
+
+    for (i = pos - 1; i < n - 1; i++)
+        a[i] = a[i + 1];
+
+    n--;
+
+    printf("Array after deletion:\n");
+    for (i = 0; i < n; i++)
+        printf("%d ", a[i]);
+
+    return 0;
+}
